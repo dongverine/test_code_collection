0a6919a37a0ab553c2683e3268317735cf0ea197
User <User@DESKTOP-KG8AIF0>
2020-02-03 17:59:35 +0900

    test

---
+++ test_spring_jdbctemplate/src/main/java/com/dongverine/jdbctemplate/TestSpringJdbctemplateApplication.java
@@ -0,0 +1,13 @@
+package com.dongverine.jdbctemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringJdbctemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringJdbctemplateApplication.class, args);
+	}
+
+}
